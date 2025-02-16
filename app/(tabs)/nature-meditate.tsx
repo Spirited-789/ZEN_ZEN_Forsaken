1274a88d22a892b75487e97cf2cabe5fe73fa141
shuraazure <hardikunova19@gmail.com>
2025-02-16 21:46:19 +0530

    changed name

--- app/(tabs)/nature-meditate.tsx
+++ app/(tabs)/nature-meditate.tsx
@@ -25,10 +25,11 @@ const Page = () => {
             >
                 <View className="mb-6">
                     <Text className="text-gray-200 mb-3 font-bold text-4xl text-left">
-                        Welcome Steven
+                        Welcome Hardik Rawat
                     </Text>
                     <Text className="text-indigo-100 text-xl font-medium">
                         Start your meditation practice today
+                        
                     </Text>
                 </View>
                 <View>
